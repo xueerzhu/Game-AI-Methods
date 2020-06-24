8b71e62d0cf759ea7d7b43b8d7b18cd4017eb5c4
Xueer Zhu <sherryxueerzhu@gmail.com>
2020-06-24 12:59:11 -0700

    Add dijkstras_shortest_path

--- Dijkstra/src/p1.py
+++ Dijkstra/src/p1.py
@@ -1,3 +1,6 @@
+
+from queue import PriorityQueue
+
 from p1_support import load_level, show_level, save_level_costs
 from math import inf, sqrt
 from heapq import heappop, heappush
@@ -17,9 +20,45 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
         Otherwise, return None.
 
     """
+    #print('dest ' + str(destination))
+    frontier = []
+    heappush(frontier, (0, initial_position))  #add initial
+    came_from = {}
+    cost_so_far = {}
+    came_from[initial_position] = None
+    cost_so_far[initial_position] = 0
 
-    
-    pass
+    while len(frontier) is not 0:
+        #print('frontier ' + str(list(frontier)))
+        current = heappop(frontier)[1] # current node coor
+
+        #print('current ' + str(current))
+
+        if current == destination:
+
+            path = []
+            # Path found building it, else return empty path
+            node = current
+            if node == destination:
+                # Go back to the top
+                while node is not None:  # while there is a parent (prev[initial_position] = None)
+                    path.append(node)
+                    node = came_from[node]  # updating to the parent
+                # Path is from dst to src, reverse it
+                path.reverse()
+            return path
+
+        for next_cor in adj(graph, current):
+            new_cost = cost_so_far[current] + next_cor[1]
+            if next_cor[0] not in cost_so_far or new_cost < cost_so_far[next_cor[0]]:
+                cost_so_far[next_cor[0]] = new_cost
+                priority = new_cost
+
+                heappush(frontier, (priority, next_cor[0]))
+
+                came_from[next_cor[0]] = current
+                #for key, value in came_from.items():
+                    #print(key, ' : ', value)
 
 
 def dijkstras_shortest_path_to_all(initial_position, graph, adj):
@@ -34,8 +73,6 @@ def dijkstras_shortest_path_to_all(initial_position, graph, adj):
         A dictionary, mapping destination cells to the cost of a path from the initial_position.
     """
     pass
-
-
 def navigation_edges(level, cell):
     """ Provides a list of adjacent cells and their respective costs from the given cell.
 
@@ -53,9 +90,48 @@ def navigation_edges(level, cell):
              ((1,1), 1.4142135623730951),
              ... ]
     """
-    pass
+    dirs = [[1, 0],  # basic ones
+            [0, 1],
+            [-1, 0],
+            [0, -1]]
+    diag = [[-1, -1],
+            [1, -1],
+            [-1, 1],
+            [1, 1]]
+    ogCellCost = 0
+    neighborCost = 0
 
+    if cell in level['spaces'].keys():
+        ogCellCost = level['spaces'].get(cell)
+    if cell in level['waypoints'].values():
+        ogCellCost = 1
+    if cell in level['walls']:
+        ogCellCost = None
 
+    result = []
+    for dir in dirs:
+        neighbor = (cell[0] + dir[0], cell[1] + dir[1])
+        if neighbor in level['spaces']:
+            neighborCost = level['spaces'].get(neighbor)
+        if neighbor in level['waypoints'].values():
+            neighborCost = 1
+        if neighbor in level['walls']:
+            neighborCost = None
+        if neighborCost and ogCellCost is not None:
+            neighborEdgeCost = (neighbor, 0.5 * (ogCellCost + neighborCost))
+            result.append(neighborEdgeCost)
+    for dir in diag:
+        neighbor = (cell[0] + dir[0], cell[1] + dir[1])
+        if neighbor in level['spaces']:
+            neighborCost = level['spaces'].get(neighbor)
+        if neighbor in level['waypoints'].values():
+            neighborCost = 1
+        if neighbor in level['walls']:
+            neighborCost = None
+        if neighborCost and ogCellCost is not None:
+            neighborEdgeCost = (neighbor, 0.5 * sqrt(2) * (ogCellCost + neighborCost))
+            result.append(neighborEdgeCost)
+    return result
 
 
 def test_route(filename, src_waypoint, dst_waypoint):
@@ -94,21 +170,21 @@ def cost_to_all_cells(filename, src_waypoint, output_filename):
         output_filename: The filename for the output csv file.
 
     """
-    
+
     # Load and display the level.
     level = load_level(filename)
     show_level(level)
 
     # Retrieve the source coordinates from the level.
     src = level['waypoints'][src_waypoint]
-    
+
     # Calculate the cost to all reachable cells from src and save to a csv file.
-    costs_to_all_cells = dijkstras_shortest_path_to_all(src, level, navigation_edges)
+    costs_to_all_cells = dijkstras_shortest_path_to_all(src, level, navigation_edges(level, src))
     save_level_costs(level, costs_to_all_cells, output_filename)
 
 
 if __name__ == '__main__':
-    filename, src_waypoint, dst_waypoint = 'example.txt', 'a','e'
+    filename, src_waypoint, dst_waypoint = 'example.txt', 'a', 'e'
 
     # Use this function call to find the route between two waypoints.
     test_route(filename, src_waypoint, dst_waypoint)
